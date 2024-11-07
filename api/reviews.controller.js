34681d85329b14a661570ab3987f8f8aff7f7085
Mahmoud Ismail <ismailm7373@gmail.com>
2024-11-07 17:32:34 +0200

    reviews.controller.js

--- api/reviews.controller.js
+++ api/reviews.controller.js
@@ -3,7 +3,7 @@ import ReviewsDAO from '../dao/reviewsDAO.js';
 export default class ReviewsController {
   static async apiPostReview(req, res, next) {
     try {
-      const bookId = parseInt(req.body.bookId);
+      const bookId = req.body.bookId;
       const review = req.body.review;
       const user = req.body.user;
 
